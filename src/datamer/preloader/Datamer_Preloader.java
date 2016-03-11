cd87006da290fab38b17020807a7d304cdf976c9
Alberto García Sánchez <agarimosoft@gmail.com>
2016-03-11 20:22:32 +0100

    WRK 201603011

--- src/datamer/preloader/Datamer_Preloader.java
+++ src/datamer/preloader/Datamer_Preloader.java
@@ -5,13 +5,18 @@
  */
 package datamer.preloader;
 
+import java.io.IOException;
 import javafx.application.Preloader;
 import javafx.application.Preloader.ProgressNotification;
 import javafx.application.Preloader.StateChangeNotification;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Simple Preloader Using the ProgressBar Control
@@ -19,34 +24,43 @@ import javafx.stage.Stage;
  * @author Agarimo
  */
 public class Datamer_Preloader extends Preloader {
-    
-    ProgressBar bar;
+
+    PreloaderC preloaderControl;
+//    ProgressBar bar;
     Stage stage;
-    
-    private Scene createPreloaderScene() {
-        bar = new ProgressBar();
-        BorderPane p = new BorderPane();
-        p.setCenter(bar);
-        return new Scene(p, 300, 150);        
+
+    private Scene createPreloaderScene() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        Pane mainPane = (Pane) loader.load(getClass().getResourceAsStream("/datamer/preloader/Preloader.fxml"));
+        PreloaderC control = loader.getController();
+        preloaderControl = control;
+
+        return new Scene(mainPane);
+
+//        bar = new ProgressBar();
+//        BorderPane p = new BorderPane();
+//        p.setCenter(bar);
+//        return new Scene(p, 300, 150);        
     }
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         this.stage = stage;
-        stage.setScene(createPreloaderScene());        
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setScene(createPreloaderScene());
         stage.show();
     }
-    
+
     @Override
     public void handleStateChangeNotification(StateChangeNotification scn) {
         if (scn.getType() == StateChangeNotification.Type.BEFORE_START) {
             stage.hide();
         }
     }
-    
+
     @Override
     public void handleProgressNotification(ProgressNotification pn) {
-        bar.setProgress(pn.getProgress());
-    }    
-    
+        preloaderControl.bar.setProgress(pn.getProgress());
+    }
+
 }
---
+++ src/datamer/preloader/PreloaderC.java
@@ -0,0 +1,34 @@
+package datamer.preloader;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Agarimo
+ */
+public class PreloaderC implements Initializable {
+    
+    @FXML
+    public ProgressBar bar;
+    
+    @FXML
+    public ImageView image;
+
+    /**
+     * Initializes the controller class.
+     * @param url
+     * @param rb
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+    }    
+    
+}
